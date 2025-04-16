71e20fa90bd8ee78a6873948372d1ff7fbf1b17f
mfeqy@youxel.com <mfeqy@youxel.com>
2025-04-16 02:19:36 +0200

    images

---
+++ PharmactMangmentEditeIdea/ViewModel/PharmacyAddMedicationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PharmactMangmentEditeIdea.ViewModel
+{
+    public class PharmacyAddMedicationDto
+    {
+        public int MedicationId { get; set; }
+        public int Quantity { get; set; }
+        public bool InStock { get; set; }
+    }
+}
